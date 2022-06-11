b72a9b7a05f07227e7a60487a3067185e27062a3
TOMER-77 <tomer-77@windowslive.com>
2022-06-11 20:36:50 +0300

    Contacts API

--- app/src/main/java/com/example/chatapp/ContactDTO.java
+++ app/src/main/java/com/example/chatapp/ContactDTO.java
@@ -2,9 +2,9 @@ package com.example.chatapp;
 
 public class ContactDTO {
 
-    public static class AddContactParams{
-        public String id ;
-        public String name ;
+    public static class AddContactParams {
+        public String id;
+        public String name;
         public String server;
 
         public AddContactParams(String id, String name, String server) {
@@ -13,4 +13,23 @@ public class ContactDTO {
             this.server = server;
         }
     }
+
+    public static class EditContactParams {
+        public String name;
+        public String server;
+
+        public EditContactParams(String name, String server) {
+            this.name = name;
+            this.server = server;
+        }
+    }
+
+    public static class MessageContent {
+        public String content;
+
+        public MessageContent(String content) {
+            this.content = content;
+        }
+    }
+
 }
